75bc0465c7fdf812005a1396361f2dc836b87bf9
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-01 20:19:47 -0600

    Initial Commit

---
+++ InstaGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaGram
+//
+//  Created by Aditya Purandare on 01/02/16.
+//  Copyright © 2016 Aditya Purandare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
