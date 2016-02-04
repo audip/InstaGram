331453cfd7c3d27f978a5db4155d00c23404454e
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-03 19:25:08 -0600

    Images load for basic view

---
+++ InstaGram/PhotoViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoViewCell.swift
+//  InstaGram
+//
+//  Created by Aditya Purandare on 03/02/16.
+//  Copyright © 2016 Aditya Purandare. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- InstaGram/PhotosViewController.swift
+++ InstaGram/PhotosViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PhotosViewController.swift
 //  InstaGram
 //
 //  Created by Aditya Purandare on 01/02/16.
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import AFNetworking
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var photos: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                        
+                        self.photos = responseDictionary["data"] as! [NSDictionary]
+                        self.tableView.reloadData()
+                }
+            }
+        });
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if let photos = photos {
+            return photos.count
+        } else {
+            return 0
+        }
+    }
+    
 
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("photoscell", forIndexPath: indexPath) as! PhotoViewCell
+        
+        let photo = photos![indexPath.row]
+        let imageUrl = NSURL(string: photo.valueForKeyPath("images.standard_resolution.url") as! String)
+        
+        cell.photoView.setImageWithURL(imageUrl!)
+                
+        //cell.textLabel!.text = "row \(indexPath.row)"
+        //print("row \(indexPath.row)")
+        
+        return cell
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
